c51ea99be6902b49bb323c5c888ada3099bbc36b
gmccormick8 <74574750+gmccormick8@users.noreply.github.com>
2025-05-08 12:44:30 -0700

    More lint debugging! :)

--- modules/compute/variables.tf
+++ modules/compute/variables.tf
@@ -13,49 +13,17 @@ variable "instance" {
     max_replicas = number
     boot_disk = object({
       image = string
-      type  = string
-      size  = number
+      type  = optional(string, "pd-standard")
+      size  = optional(number, 50)
     })
     network_interface = object({
       subnetwork = string
-      network_ip = string
+      network_ip = optional(string)
     })
-    service_account = object({
-      email  = string
-      scopes = list(string)
-    })
-    tags = list(string)
+    service_account = optional(object({
+      email  = optional(string)
+      scopes = optional(list(string), ["cloud-platform"])
+    }))
+    tags = optional(list(string), [])
   })
-
-  validation {
-    condition     = var.instance.min_replicas > 0
-    error_message = "min_replicas must be greater than 0"
-  }
-
-  validation {
-    condition     = var.instance.max_replicas >= var.instance.min_replicas
-    error_message = "max_replicas must be greater than or equal to min_replicas"
-  }
-
-  default = {
-    name_prefix  = null # Required - must be provided
-    machine_type = null # Required - must be provided
-    region       = null # Required - must be provided
-    min_replicas = null # Required - must be provided
-    max_replicas = null # Required - must be provided
-    boot_disk = {
-      image = null # Required - must be provided
-      type  = "pd-standard"
-      size  = 50
-    }
-    network_interface = {
-      subnetwork = null # Required - must be provided
-      network_ip = null
-    }
-    service_account = {
-      email  = null
-      scopes = ["cloud-platform"]
-    }
-    tags = []
-  }
 }
